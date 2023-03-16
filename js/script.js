939b81db82c42e58b02d57e40aa741c8e6b762c2
rjsander <rjsander19@gmail.com>
2023-03-16 11:47:17 -0700

    commit with everything functioning

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let weatherData, userInput;
+
+const $temp= $('#temp')
+const $feels_like = $('#feels_like')
+const $humidity = $('#humidity')
+const $input = $('input[type="text"]')
+
+
+$('form').on('submit', handleGetData);
+
+
+function handleGetData(event) {
+    event.preventDefault();
+    userInput = $input.val();
+$.ajax({
+    url:`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=b7819fb3ac46283d9eac8fba5a615b18`
+
+
+}).then(
+  (data) => {(console.log(data))
+   weatherData = data;
+   render();
+  },
+  (error) => {
+   console.log('bad request: ', error);
+  }
+)
+}
+
+function render() {
+    $temp.text(weatherData.main.temp)
+    $feels_like.text(weatherData.main.feels_like)
+    $humidity.text(weatherData.main.humidity)
+}
+
+
+// url:`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${apiKey}`
+
+
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b7819fb3ac46283d9eac8fba5a615b18
+
+// b7819fb3ac46283d9eac8fba5a615b18
